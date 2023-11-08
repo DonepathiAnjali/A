481ec131ffbb423752ad04f8ee7531e399bcd774
pc <pc@Anjali>
2023-11-08 17:59:18 +0530

    BrokenLinks

--- Hybrid/src/main/java/Testng/BrokenLinks.java
+++ Hybrid/src/main/java/Testng/BrokenLinks.java
@@ -16,50 +16,60 @@ import org.testng.annotations.Test;
 
 public class BrokenLinks {
 
-    @Test 
-    public void main() throws Exception{
+	@Test 
+	public void main() throws Exception{
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\pc\\Downloads\\chromedriver-win64\\chromedriver-win64/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\pc\\Downloads\\chromedriver-win64\\chromedriver-win64/chromedriver.exe");
 
-        WebDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 
-        driver.manage().window().maximize();
+		driver.manage().window().maximize();
 
-        driver.get("https://www.miniso.com/");
+		driver.get("https://www.miniso.com/");
 
-        driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+		WebDriverWait wait = new WebDriverWait(driver, 10);
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("a")));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("a")));
 
-        List<WebElement> links = driver.findElements(By.tagName("a"));
+		List<WebElement> links = driver.findElements(By.tagName("a"));
 
-        System.out.println(links.size());
+		System.out.println(links.size());
 
-        for (WebElement link : links) {
+		for (WebElement link : links) {
 
-            String linkURL = link.getAttribute("href");
+			String linkURL = link.getAttribute("href");
 
-            try {
-                URL url = new URL(linkURL);
-                URLConnection urlConnection = url.openConnection();
-                HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
-                httpURLConnection.setConnectTimeout(5000);
-                httpURLConnection.connect();
+			try {
+				URL url = new URL(linkURL);
 
-                if (httpURLConnection.getResponseCode() > 200) {
-                    System.out.println(linkURL + " - " + httpURLConnection.getResponseMessage());
-                }
+				URLConnection urlConnection = url.openConnection();
 
-                httpURLConnection.disconnect();
-            } catch(MalformedURLException e) {
-                System.out.println(linkURL + " - Malformed URL: " + e.getMessage());
-            } catch (Exception e) {
-                System.out.println(linkURL + " - Error: " + e.getMessage());
-            }
-        }
+				HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
 
-        driver.quit();
-    }
+				httpURLConnection.setConnectTimeout(5000);
+
+				httpURLConnection.connect();
+
+				if (httpURLConnection.getResponseCode() > 200) {
+					
+					System.out.println(linkURL + " - " + httpURLConnection.getResponseMessage());
+				}
+
+				httpURLConnection.disconnect();
+
+			}
+			catch(MalformedURLException e) {
+
+				System.out.println(linkURL + " - Malformed URL: " + e.getMessage());
+
+			} catch (Exception e) {
+
+				System.out.println(linkURL + " - Error: " + e.getMessage());
+			}
+		}
+
+		driver.quit();
+	}
 }
