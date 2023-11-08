6dc7dadc168cc181246f75b7d8f7f8036cd5a8b8
pc <pc@Anjali>
2023-11-08 17:31:30 +0530

    brokenlinks

---
+++ Hybrid/src/main/java/Testng/BrokenLinks.java
@@ -0,0 +1,65 @@
+package Testng;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException; // Add import statement for MalformedURLException
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class BrokenLinks {
+
+    @Test 
+    public void main() throws Exception{
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\pc\\Downloads\\chromedriver-win64\\chromedriver-win64/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.miniso.com/");
+
+        driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("a")));
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        System.out.println(links.size());
+
+        for (WebElement link : links) {
+
+            String linkURL = link.getAttribute("href");
+
+            try {
+                URL url = new URL(linkURL);
+                URLConnection urlConnection = url.openConnection();
+                HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.connect();
+
+                if (httpURLConnection.getResponseCode() > 200) {
+                    System.out.println(linkURL + " - " + httpURLConnection.getResponseMessage());
+                }
+
+                httpURLConnection.disconnect();
+            } catch(MalformedURLException e) {
+                System.out.println(linkURL + " - Malformed URL: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println(linkURL + " - Error: " + e.getMessage());
+            }
+        }
+
+        driver.quit();
+    }
+}
